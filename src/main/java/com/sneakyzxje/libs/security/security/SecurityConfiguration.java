a9260b9483b34ebac18d39fa2463d782a9a2fab8
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-09 22:45:31 +0700

    feat(security): add security configuration

---
+++ src/main/java/com/sneakyzxje/libs/security/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.sneakyzxje.libs.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sneakyzxje.libs.security.filter.JwtFilter;
+import com.sneakyzxje.libs.security.properties.SecurityProperties;
+
+import lombok.RequiredArgsConstructor;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final SecurityProperties securityProperties;
+    private final JwtFilter jwtFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String[] publicEndpoints = securityProperties.getEndpoints().toArray(new String[0]);
+
+        http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> 
+            auth.requestMatchers(publicEndpoints).permitAll()
+            .anyRequest().authenticated()
+        );
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
