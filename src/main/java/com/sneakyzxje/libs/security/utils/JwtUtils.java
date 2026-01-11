3188747aa22ea4e48f17f2c42d9a8be32ccf3e80
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-12 02:47:34 +0700

    feat(security): split access token creation and add refresh token generation

--- src/main/java/com/sneakyzxje/libs/security/utils/JwtUtils.java
+++ src/main/java/com/sneakyzxje/libs/security/utils/JwtUtils.java
@@ -4,12 +4,16 @@ import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
+import com.sneakyzxje.libs.security.dto.RefreshTokenDTO;
 import com.sneakyzxje.libs.security.properties.SecurityProperties;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -44,14 +48,22 @@ public class JwtUtils {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
-            return true;
+        Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token);
+        
+        return true;
+        } catch (ExpiredJwtException e) {
+            System.out.println("Token expired! " + e.getMessage());
+            return false; 
         } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
             return false;
         }
     }
 
-    public String createToken(String subject, List<String> roles) {
+    public String createAccessToken(String subject, List<String> roles) {
         long now = System.currentTimeMillis();
         long exp = now + (securityProperties.getAuthentication().getJwtExpiration() * 1000);
         Date issuedAt = new Date(now);
@@ -64,4 +76,14 @@ public class JwtUtils {
         .setExpiration(expiredAt)
         .compact();
     }
+
+    public RefreshTokenDTO createRefreshToken() {
+        String refreshToken = UUID.randomUUID().toString();
+        long now = System.currentTimeMillis();
+        long exp = now + (securityProperties.getAuthentication().getRefreshTokenExpiration() * 1000);
+        Date expiryDate = new Date(exp);
+
+        return new RefreshTokenDTO(refreshToken, expiryDate);
+    }
+
 }
